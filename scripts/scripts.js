a80ab6566a759124afb2f124b21e55b956ffb1aa
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2022-08-18 15:31:23 -0300

    Projeto finalizado

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,7 +4,44 @@ let tocador = "";
 function playSound(soundId) {
     tocador = document.getElementById(soundId);
     tocador.play();
-    document.getElementById('display').innerHTML = nomesSom[0];
+    displaySound(soundId.toLowerCase());
 }
 
+document.addEventListener("keyup", function(event) {
+    displaySound(event.key);
+    playSound(event.key.toUpperCase());
+});
 
+function displaySound(valor) {
+    switch (valor) {
+        case "q":
+            document.getElementById('display').innerHTML = nomesSom[0];
+            break;
+        case "w":
+            document.getElementById('display').innerHTML = nomesSom[1];
+            break;
+        case "e":
+            document.getElementById('display').innerHTML = nomesSom[2];
+            break;
+        case "a":
+            document.getElementById('display').innerHTML = nomesSom[3];
+            break;
+        case "s":
+            document.getElementById('display').innerHTML = nomesSom[4];
+            break;
+        case "d":
+            document.getElementById('display').innerHTML = nomesSom[5];
+            break;
+        case "z":
+            document.getElementById('display').innerHTML = nomesSom[6];
+            break;
+        case "x":
+            document.getElementById('display').innerHTML = nomesSom[7];
+            break;
+        case "c":
+            document.getElementById('display').innerHTML = nomesSom[8];
+            break;
+        default:
+            break;
+    }
+}
